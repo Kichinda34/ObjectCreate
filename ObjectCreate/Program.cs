f150b3d5a94ec70940fe1f817dc1d450391bef63
Kichinda34 <leandroschneiderlima@gmail.com>
2023-03-10 12:57:52 -0300

    Add project files.

---
+++ ObjectCreate/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    public class Pessoa
+    {
+        string nome;
+
+        //public int idade { get; set; }
+        //public int altura { get; set; }
+
+        public Pessoa() { }
+
+        public void DefinirNome (string nome) 
+        {
+            this.nome = nome;
+        }
+
+        public string RetornarNome() 
+        {
+            return this.nome;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Pessoa p = new Pessoa();
+        Pessoa p2 = new Pessoa();
+
+        Console.WriteLine("Informe seu nome: ");
+        p.DefinirNome(Console.ReadLine());
+
+        Console.WriteLine("Informe seu nome: ");
+        p2.DefinirNome(Console.ReadLine());
+
+
+        Console.WriteLine("Seja bem vindo a orientação a objetivos, " + p.RetornarNome());
+        Console.WriteLine("Seja bem vindo a orientação a objetivos, " + p2.RetornarNome());
+
+
+    }
+}
